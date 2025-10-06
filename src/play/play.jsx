fc1155409f297a1196e3137bbb6777c8c179c8e1
Patrick Blood <pblood66@gmail.com>
2025-10-06 13:57:47 -0600

    Added play page

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -2,8 +2,35 @@ import React from 'react'
 
 export function Play() {
     return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>play displayed here</div>
+    <main className="container-fluid text-center">
+      <div class="game-stats">
+                <div class=""player-name>
+                    Player: 
+                    <span class="player-name">generic_user</span>
+                </div>
+                <div class="score">
+                    <label for="score-count">IQ:</label>
+                    <input type="text" id="score-count" value="--" readonly />
+                </div>
+            </div>
+
+            <div class="notifications">
+                <div class="event"><span class="player-event">Bobby</span> started a game</div>    
+                <div class="event"><span class="player-event">Bobby</span> scored 27 pts</div>
+                <div class="event"><span class="player-event">Jimmy</span> started a game</div>    
+            </div>
+            
+            <div class="trivia-question">
+                <h2>What is the capital of France?</h2>   
+                <div class="answers">
+                    <form method="get" action="game-over.html">
+                        <button type="submit" id="A" class="btn btn-primary btn-lg btn-block">Answer for A goes here</button>
+                        <button type="submit" id="B" class="btn btn-primary btn-lg btn-block">Answer for B goes here</button>
+                        <button type="submit" id="C" class="btn btn-primary btn-lg btn-block">Answer for C goes here</button>
+                        <button type="submit" id="D" class="btn btn-primary btn-lg btn-block">Answer for D goes here</button>
+                    </form>
+                </div> 
+            </div>
     </main>
     );
 }
--- src/scores/scores.jsx
+++ src/scores/scores.jsx
@@ -1,9 +1,68 @@
 import React from 'react'
+import './scores.css'
 
 export function Scores() {
     return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>scores displayed here</div>
-    </main>
+    <main class="container-fluid text-center">
+      <div class="game-stats">
+                  <div class=""player-name>
+                      Player: 
+                      <span class="player-name">generic_user</span>
+                  </div>
+              </div>
+              <div class="notifications">
+                  <div class="event"><span class="player-event">Bobby</span> started a game</div>    
+                  <div class="event"><span class="player-event">Bobby</span> scored 27 pts</div>
+                  <div class="event"><span class="player-event">Jimmy</span> started a game</div>    
+              </div>
+
+              <div class="scores">
+                  <h2>High Scores</h2>
+                  <table>
+                      <tbody>
+                          <tr>
+                              <td class="player-name">1. Billy</td>
+                              <td class="score">256</td>
+                          </tr>
+                          <tr>
+                              <td class="player-name">2. Bobby</td>
+                              <td class="score">251</td>
+                          </tr>
+                          <tr>    
+                              <td class="player-name">3. Johnny</td>
+                              <td class="score">189</td>
+                          </tr>
+                          <tr>
+                              <td class="player-name">4. Kyle</td>
+                              <td class="score">175</td>
+                          </tr>
+                          <tr>
+                              <td class="player-name">5. Billy</td>
+                              <td class="score">142</td>
+                          </tr>
+                          <tr>
+                              <td class="player-name">6. Bobby</td>
+                              <td class="score">121</td>
+                          </tr>
+                          <tr>
+                              <td class="player-name">7. Tommy</td>
+                              <td class="score">90</td>
+                          </tr>
+                          <tr>
+                              <td class="player-name">8. Johnny</td>
+                              <td class="score">54</td>
+                          </tr>
+                          <tr>
+                              <td class="player-name">9. Sammy</td>
+                              <td class="score">33</td>
+                          </tr>
+                          <tr>
+                              <td class="player-name">10. Tommy</td>
+                              <td class="score">17</td>
+                          </tr>
+                      </tbody>
+                  </table>
+              </div>
+      </main>
     );
 }
