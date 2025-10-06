5675674536855a385a60211f45a1a7daca876bfb
Patrick Blood <pblood66@gmail.com>
2025-10-06 13:15:09 -0600

    Added Routing

--- src/app.jsx
+++ src/app.jsx
@@ -1,35 +1,71 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { About } from './about/about';
+
 import './app.css';
 
 export default function App() {
   return (
-    <div>
-      <header class="container-fluid">
-          <div class="brand">
-              <h1>Trivia Run</h1>
-              <img src="logo.jpg" width="80px" />
-          </div>
-          
-          <nav >
-              <ul>
-                  <li class="menu-item"><a class="nav-link" href="index.html">Home</a></li>
-                  <li class="menu-item"><a class="nav-link" href="play.html">Play</a></li>
-                  <li class="menu-item"><a class="nav-link" href="scores.html">High Scores</a></li>
-                  <li class="menu-item"><a class="nav-link" href="about.html">About</a></li>
+    <BrowserRouter>
+      <div>
+        <header className="container-fluid">
+            <div className="brand">
+                <h1>Trivia Run</h1>
+                <img src="logo.jpg" width="80px" alt="logo" />
+            </div>
+            
+            <nav>
+                <ul>
+                    <li className="menu-item">
+                      <NavLink to="/" end className="nav-link">
+                        Home
+                      </NavLink>
+                    </li>
+                    <li className="menu-item">
+                      <NavLink to="/play" className="nav-link">
+                        Play
+                      </NavLink>
+                    </li>
+                    <li className="menu-item">
+                      <NavLink to="/scores" className="nav-link">
+                        High Scores
+                      </NavLink>
+                    </li>
+                    <li className="menu-item">
+                      <NavLink to="/about" className="nav-link">
+                        About
+                      </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
 
-              </ul>
-          </nav>
-      </header>
+        <main>App components go here</main>
 
-      <main>App components go here</main>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/play" element={<Play />} />
+          <Route path="/scores" element={<Scores />} />
+          <Route path="/about" element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
 
-      <footer>       
-        <div class="container-fluid">
-          <span class="text-reset">Patrick Blood</span>
-          <a class="text-reset" href="https://github.com/pblood66/startup">GitHub</a>
-        </div>  
-      </footer>
-    </div>
+        <footer>       
+          <div className="container-fluid">
+            <span className="text-reset">Patrick Blood</span>
+            <a className="text-reset" href="https://github.com/pblood66/startup">GitHub</a>
+          </div>  
+        </footer>
+      </div>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
---
+++ src/game-over/game-over.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Game_Over() {
+    return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>game over displayed here</div>
+    </main>
+    );
+}
