9def7177eb881c040037d94760a871cae4122255
Patrick Blood <pblood66@gmail.com>
2025-10-20 20:45:35 -0600

    Added high score table that uses local storage

--- src/game-over/game-over.jsx
+++ src/game-over/game-over.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import "./game-over.css"
 import "../scores/scores.css"
+import { SocketNotifications } from '../play/notifications';
 
 
 export function Game_Over() {
+  const [scores, setScores] = React.useState([]);
+
+  React.useEffect(() => {
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+      setScores(JSON.parse(scoresText));
+    }
+  }, []);
+
+  // Sort descending and limit to top 10
+  const sortedScores = [...scores].sort((a, b) => b.score - a.score).slice(0, 10);
+
+  const scoreRows = [];
+  if (sortedScores.length) {
+    for (const [i, score] of sortedScores.entries()) {
+      scoreRows.push(
+        <tr key={i}>
+          <td>{i + 1}</td>
+          <td>{score.name.split('@')[0]}</td>
+          <td>{score.score}</td>
+        </tr>
+      );
+    }
+  } else {
+    scoreRows.push(
+      <tr key="0">
+        <td colSpan="4">Be the first to score</td>
+      </tr>
+    );
+  }
+
     return (
     <main className="container-fluid text-center">
         <div className="game-stats">
@@ -13,68 +45,28 @@ export function Game_Over() {
             </div>
         </div>
 
-        <div className="notifications">
-            <div className="event"><span className="player-event">Bobby</span> started a game</div>    
-            <div className="event"><span className="player-event">Bobby</span> scored 27 pts</div>
-            <div className="event"><span className="player-event">Jimmy</span> started a game</div>    
-        </div>
+        <SocketNotifications />
+
         <div className="game-over-title">
             <h1>Game Over</h1>
             <img src="sad-emoji.webp" width="100px" />
         </div>
         <div className="scores">
-          <h2>High Scores</h2>
-          <table>
-              <tbody>
-                  <tr>
-                      <td className="player-name">1. Billy</td>
-                      <td className="score">256</td>
-                  </tr>
-                  <tr>
-                      <td className="player-name">2. Bobby</td>
-                      <td className="score">251</td>
-                  </tr>
-                  <tr>    
-                      <td className="player-name">3. Johnny</td>
-                      <td className="score">189</td>
-                  </tr>
-                  <tr>
-                      <td className="player-name">4. Kyle</td>
-                      <td className="score">175</td>
-                  </tr>
-                  <tr>
-                      <td className="player-name">5. Billy</td>
-                      <td className="score">142</td>
-                  </tr>
-                  <tr>
-                      <td className="player-name">6. Bobby</td>
-                      <td className="score">121</td>
-                  </tr>
-                  <tr>
-                      <td className="player-name">7. Tommy</td>
-                      <td className="score">90</td>
-                  </tr>
-                  <tr>
-                      <td className="player-name">8. Johnny</td>
-                      <td className="score">54</td>
-                  </tr>
-                  <tr>
-                      <td className="player-name">9. Sammy</td>
-                      <td className="score">33</td>
-                  </tr>
-                  <tr>
-                      <td className="player-name">10. Tommy</td>
-                      <td className="score">17</td>
-                  </tr>
-              </tbody>
-          </table>
-      </div>
-      <div className="play-again">
-        <form method="get" action="./play">
-          <button type="submit" className="btn btn-primary">Play Again</button>
-        </form>
-      </div>
+            <h2>High Scores</h2>
+            <table>
+                <thead>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Score</th>
+                </thead>
+                <tbody id='scores'>{scoreRows}</tbody>
+            </table>
+        </div>
+        <div className="play-again">
+            <form method="get" action="./play">
+                <button type="submit" className="btn btn-primary">Play Again</button>
+            </form>
+        </div>
     </main>
-    
     );
 }
--- src/scores/scores.jsx
+++ src/scores/scores.jsx
@@ -3,74 +3,60 @@ import './scores.css'
 
 import { SocketNotifications } from '../play/notifications';
 
-export function Scores() {
-    const [scores, setScores] = React.useState([])
+export function Scores() {  
+    const [scores, setScores] = React.useState([]);
 
     React.useEffect(() => {
-    const scoresText = localStorage.getItem('scores');
-    if (scoresText) {
-      setScores(JSON.parse(scoresText));
-    }
-  }, []);
+        const scoresText = localStorage.getItem('scores');
+        if (scoresText) {
+        setScores(JSON.parse(scoresText));
+        }
+    }, []);
 
+    // Sort descending and limit to top 10
+    const sortedScores = [...scores].sort((a, b) => b.score - a.score).slice(0, 10);
+
+    const scoreRows = [];
+    if (sortedScores.length) {
+        for (const [i, score] of sortedScores.entries()) {
+        scoreRows.push(
+            <tr key={i}>
+            <td>{i + 1}</td>
+            <td>{score.name.split('@')[0]}</td>
+            <td>{score.score}</td>
+            </tr>
+        );
+        }
+    } else {
+        scoreRows.push(
+        <tr key="0">
+            <td colSpan="4">Be the first to score</td>
+        </tr>
+        );
+    }
+        
     return (
     <main className="container-fluid text-center">
-      <div className="game-stats">
-                  <div className=""player-name>
-                      Player: 
-                      <span className="player-name">generic_user</span>
-                  </div>
-              </div>
+        <div className="game-stats">
+            <div className=""player-name>
+                Player: 
+                <span className="player-name">generic_user</span>
+            </div>
+        </div>
               
-              <SocketNotifications />
+        <SocketNotifications />   
 
-              <div className="scores">
-                  <h2>High Scores</h2>
-                  <table>
-                      <tbody>
-                          <tr>
-                              <td className="player-name">1. Billy</td>
-                              <td className="score">256</td>
-                          </tr>
-                          <tr>
-                              <td className="player-name">2. Bobby</td>
-                              <td className="score">251</td>
-                          </tr>
-                          <tr>    
-                              <td className="player-name">3. Johnny</td>
-                              <td className="score">189</td>
-                          </tr>
-                          <tr>
-                              <td className="player-name">4. Kyle</td>
-                              <td className="score">175</td>
-                          </tr>
-                          <tr>
-                              <td className="player-name">5. Billy</td>
-                              <td className="score">142</td>
-                          </tr>
-                          <tr>
-                              <td className="player-name">6. Bobby</td>
-                              <td className="score">121</td>
-                          </tr>
-                          <tr>
-                              <td className="player-name">7. Tommy</td>
-                              <td className="score">90</td>
-                          </tr>
-                          <tr>
-                              <td className="player-name">8. Johnny</td>
-                              <td className="score">54</td>
-                          </tr>
-                          <tr>
-                              <td className="player-name">9. Sammy</td>
-                              <td className="score">33</td>
-                          </tr>
-                          <tr>
-                              <td className="player-name">10. Tommy</td>
-                              <td className="score">17</td>
-                          </tr>
-                      </tbody>
-                  </table>
-              </div>
-      </main>
+        <div className="scores">
+          <h2>High Scores</h2>
+            <table>
+                <thead>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Score</th>
+                </thead>
+                <tbody id='scores'>{scoreRows}</tbody>
+            </table>
+        </div>
+    </main>
     );
 }
