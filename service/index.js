08a55bbeb7c0b96660a7ea79e22101d814fc9635
pblood66 <pblood66@gmail.com>
2025-11-03 08:47:16 -0700

    register user service function

--- service/index.js
+++ service/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
-// const uuid = require('uuid');
+const uuid = require('uuid');
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
@@ -13,6 +13,24 @@ app.use(express.json());
 
 let apiRouter = express.Router();
 app.use(`/api`, apiRouter);
+
+apiRouter.post('/auth/create', async (req, res) => {
+
+  const user = await createServerModuleRunner(req.body.email, req.body.password);
+});
+
+async function createUser(email, password) {
+  const hash = await bcrypt.hash(password, 16);
+
+  const user = {
+    email: email,
+    password: hash,
+    authToken: uuid.v4(), 
+  };
+
+  users.push(user);
+  return user;
+}
 
 
 app.listen(port, () => {
