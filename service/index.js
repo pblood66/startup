b75b3ec9248b1e5c0387ec41ac9c31f3394b6940
pblood66 <pblood66@gmail.com>
2025-11-04 14:40:20 -0700

    update deploy file

--- service/index.js
+++ service/index.js
@@ -112,7 +112,7 @@ async function findUser(field, value) {
 function setAuthCookie(res, authToken) {
   res.cookie(authCookieName, authToken, {
     maxAge: 1000 * 60 * 60 * 24 * 365,
-    secure: false,
+    secure: true,
     httpOnly: true,
     sameSite: 'strict',
   });
